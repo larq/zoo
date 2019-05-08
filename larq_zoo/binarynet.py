aeea1a14b3b117a14029661f698ebd8746dcc7e4
Lukas Geiger <lukas.geiger94@gmail.com>
2019-05-08 16:57:28 +0100

    Add initial version of BinaryNet on Imagenet

--- larq_zoo/binarynet.py
+++ larq_zoo/binarynet.py
@@ -1,15 +1,63 @@
 from larq_flock import registry, HParams
+import larq as lq
+import tensorflow as tf
 
 
 @registry.register_model
-def binarynet(hparams, dataset):
-    pass
+def binary_alex_net(hparams, dataset):
+    kwargs = dict(
+        input_quantizer="ste_sign",
+        kernel_quantizer="ste_sign",
+        kernel_constraint="weight_clip",
+        use_bias=False,
+    )
+
+    return tf.keras.models.Sequential(
+        [
+            lq.layers.QuantConv2D(
+                hparams.filters,
+                3,
+                kernel_quantizer="ste_sign",
+                kernel_constraint="weight_clip",
+                use_bias=False,
+                input_shape=dataset.input_shape,
+            ),
+            tf.keras.layers.BatchNormalization(scale=False),
+            lq.layers.QuantConv2D(hparams.filters, 3, padding="same", **kwargs),
+            tf.keras.layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2)),
+            tf.keras.layers.BatchNormalization(scale=False),
+            lq.layers.QuantConv2D(2 * hparams.filters, 3, padding="same", **kwargs),
+            tf.keras.layers.BatchNormalization(scale=False),
+            lq.layers.QuantConv2D(2 * hparams.filters, 3, padding="same", **kwargs),
+            tf.keras.layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2)),
+            tf.keras.layers.BatchNormalization(scale=False),
+            lq.layers.QuantConv2D(4 * hparams.filters, 3, padding="same", **kwargs),
+            tf.keras.layers.BatchNormalization(scale=False),
+            lq.layers.QuantConv2D(4 * hparams.filters, 3, padding="same", **kwargs),
+            tf.keras.layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2)),
+            tf.keras.layers.BatchNormalization(scale=False),
+            tf.keras.layers.Flatten(),
+            lq.layers.QuantDense(hparams.dense_units, **kwargs),
+            tf.keras.layers.BatchNormalization(scale=False),
+            lq.layers.QuantDense(hparams.dense_units, **kwargs),
+            tf.keras.layers.BatchNormalization(scale=False),
+            lq.layers.QuantDense(dataset.num_classes, **kwargs),
+            tf.keras.layers.BatchNormalization(scale=False),
+            tf.keras.layers.Activation("softmax"),
+        ]
+    )
 
 
-@registry.register_hparams(binarynet)
+@registry.register_hparams(binary_alex_net)
 def default():
-    return HParams()
+    return HParams(
+        optimizer=tf.keras.optimizers.Adam,
+        learning_rate=1e-3,
+        batch_size=256,
+        filters=128,
+        dense_units=1024,
+    )
 
 
-def BinaryNet():
+def BinaryAlexNet():
     raise NotImplementedError()
--- larq_zoo/train.py
+++ larq_zoo/train.py
@@ -1,10 +1,45 @@
 from larq_flock import cli, build_train
+import click
 
 
 @cli.command()
+@click.option("--tensorboard/--no-tensorboard", default=True)
 @build_train
-def train(build_model, dataset, hparams, output_dir, epochs):
-    pass
+def train(build_model, dataset, hparams, output_dir, epochs, tensorboard):
+    import larq as lq
+    from larq_zoo.utils import get_distribution_scope
+    import tensorflow as tf
+
+    callbacks = [lq.callbacks.QuantizationLogger(update_freq=1000)]
+    if tensorboard:
+        callbacks.append(
+            tf.keras.callbacks.TensorBoard(
+                log_dir=output_dir,
+                write_graph=False,
+                update_freq=1000,
+                histogram_freq=10,
+            )
+        )
+
+    with get_distribution_scope(hparams.batch_size):
+        model = build_model(hparams, dataset)
+        model.compile(
+            optimizer=hparams.optimizer(hparams.learning_rate),
+            loss="categorical_crossentropy",
+            metrics=["categorical_accuracy", "top_k_categorical_accuracy"],
+        )
+
+    lq.models.summary(model)
+
+    model.fit(
+        dataset.train_data(hparams.batch_size),
+        epochs=epochs,
+        steps_per_epoch=dataset.train_examples // hparams.batch_size,
+        validation_data=dataset.validation_data(hparams.batch_size),
+        validation_steps=dataset.validation_examples // hparams.batch_size,
+        verbose=2 if tensorboard else 1,
+        callbacks=callbacks,
+    )
 
 
 if __name__ == "__main__":
