f484c3f8b0034e7f53a20197fc28f37a98332f88
Lukas Geiger <lukas.geiger94@gmail.com>
2019-05-11 02:41:50 +0100

    Add model tests similar to keras applications

---
+++ tests/models_test.py
@@ -0,0 +1,52 @@
+import pytest
+import functools
+import larq_zoo as lqz
+from tensorflow.keras.backend import clear_session
+
+
+def keras_test(func):
+    """Function wrapper to clean up after TensorFlow tests.
+    # Arguments
+        func: test function to clean up after.
+    # Returns
+        A function wrapping the input function.
+    """
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        output = func(*args, **kwargs)
+        clear_session()
+        return output
+
+    return wrapper
+
+
+def parametrize(func):
+    func = keras_test(func)
+    return pytest.mark.parametrize("app,last_dim", [(lqz.BinaryAlexNet, 256)])(func)
+
+
+@parametrize
+def test_basic(app, last_dim):
+    model = app(weights=None)
+    assert model.output_shape == (None, 1000)
+
+
+@parametrize
+def test_no_top(app, last_dim):
+    model = app(weights=None, include_top=False)
+    assert model.output_shape == (None, None, None, last_dim)
+
+
+@parametrize
+def test_no_top_variable_shape_1(app, last_dim):
+    input_shape = (None, None, 1)
+    model = app(weights=None, include_top=False, input_shape=input_shape)
+    assert model.output_shape == (None, None, None, last_dim)
+
+
+@parametrize
+def test_no_top_variable_shape_4(app, last_dim):
+    input_shape = (None, None, 4)
+    model = app(weights=None, include_top=False, input_shape=input_shape)
+    assert model.output_shape == (None, None, None, last_dim)
