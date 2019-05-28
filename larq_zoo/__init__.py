52a32b63640a61db0e01b61c193d34c4920fe101
Lukas Geiger <lukas.geiger94@gmail.com>
2019-05-28 19:00:01 +0100

    Add initial implementation of Bi-Real Net

--- larq_zoo/__init__.py
+++ larq_zoo/__init__.py
@@ -1,4 +1,5 @@
 from larq_zoo.binarynet import BinaryAlexNet
+from larq_zoo.birealnet import BiRealNet
 from larq_zoo.data import default as preprocess_imagenet
 
-__all__ = ["BinaryAlexNet", "preprocess_imagenet"]
+__all__ = ["BinaryAlexNet", "BiRealNet", "preprocess_imagenet"]
