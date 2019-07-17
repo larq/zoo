1570c0b090005503f750d9c7fa1f85d0e64aef57
Lukas Geiger <lukas.geiger94@gmail.com>
2019-07-17 11:04:01 +0100

    Add missing scipy test dependency

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     extras_require={
         "tensorflow": ["tensorflow>=1.13.1"],
         "tensorflow_gpu": ["tensorflow-gpu>=1.13.1"],
-        "test": ["pytest>=4.3.1", "pytest-cov>=2.6.1", "Pillow==6.1.0"],
+        "test": ["pytest>=4.3.1", "pytest-cov>=2.6.1", "Pillow==6.1.0", "scipy==1.3.0"],
     },
     entry_points="""
         [console_scripts]
