085f2ba1a845f7176ef0080f83308c9abe9e64f0
Lukas Geiger <lgeiger@users.noreply.github.com>
2019-07-17 13:32:00 +0100

    Add integration test for training loop (#38)
    * Add integration test for training loop
    
    This test will take around 5 minutes which isn't ideal, but I think it is a useful sanity check to have.
    
    * Add missing scipy test dependency
    
    * :arrrow_up: zookeeper@~0.3.1

--- larq_zoo/data.py
+++ larq_zoo/data.py
@@ -48,7 +48,7 @@ def preprocess_input(image):
 
 
 @registry.register_preprocess("imagenet2012", (224, 224, 3))
-@registry.register_preprocess("oxford_iiit_pet", (224, 224, 3))
+@registry.register_preprocess("oxford_flowers102", (224, 224, 3))  # For testing
 def default(image, training):
     return preprocess_image(image, 224, 224, is_training=training)
 
