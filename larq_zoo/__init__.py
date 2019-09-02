29fd637bd47a8ecf13948f9ca4af227d98c321b8
Lukas Geiger <lgeiger@users.noreply.github.com>
2019-09-02 14:33:23 +0100

    Add Dilated BinaryDenseNet37 (#66)
    * Add Dilated BinaryDenseNet37
    
    * Skipt train test on TF2 due to https://github.com/larq/larq/pull/195

--- larq_zoo/__init__.py
+++ larq_zoo/__init__.py
@@ -2,7 +2,12 @@ from larq_zoo.binarynet import BinaryAlexNet
 from larq_zoo.birealnet import BiRealNet
 from larq_zoo.xnornet import XNORNet
 from larq_zoo.resnet_e import BinaryResNetE18
-from larq_zoo.densenet import BinaryDenseNet28, BinaryDenseNet37, BinaryDenseNet45
+from larq_zoo.densenet import (
+    BinaryDenseNet28,
+    BinaryDenseNet37,
+    BinaryDenseNet37Dilated,
+    BinaryDenseNet45,
+)
 from larq_zoo.data import preprocess_input
 from tensorflow.keras.applications.vgg16 import decode_predictions
 
@@ -13,6 +18,7 @@ __all__ = [
     "BinaryResNetE18",
     "BinaryDenseNet28",
     "BinaryDenseNet37",
+    "BinaryDenseNet37Dilated",
     "BinaryDenseNet45",
     "decode_predictions",
     "preprocess_input",
