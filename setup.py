8d7abbbe96bd8ec5da08a7c02cf3e3aa89c86179
Lukas Geiger <lukas.geiger94@gmail.com>
2019-09-02 18:03:16 +0100

    :arrow_up: 0.4.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name="larq-zoo",
-    version="0.3.0",
+    version="0.4.0",
     author="Plumerai",
     author_email="lukas@plumerai.co.uk",
     description="Reference implementations of popular Binarized Neural Networks",
