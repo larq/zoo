6b0893f8d74292abe8164f204ff9d154dd3ef3e3
koenhelwegen <koen@plumerai.com>
2020-02-21 13:19:10 +0000

    move files to right folders and modify __init__.py files

--- larq_zoo/__init__.py
+++ larq_zoo/__init__.py
@@ -1,31 +1,7 @@
 from larq_zoo import datasets
-from larq_zoo.binary_alex_net import BinaryAlexNet
-from larq_zoo.birealnet import BiRealNet
 from larq_zoo.data import preprocess_input
-from larq_zoo.densenet import (
-    BinaryDenseNet28,
-    BinaryDenseNet37,
-    BinaryDenseNet37Dilated,
-    BinaryDenseNet45,
-)
-from larq_zoo.dorefanet import DoReFaNet
-from larq_zoo.quicknet import QuickNet
-from larq_zoo.resnet_e import BinaryResNetE18
 from larq_zoo.utils import decode_predictions
-from larq_zoo.xnornet import XNORNet
+import larq_zoo.literature as literature
+import larq_zoo.sota as sota
 
-__all__ = [
-    "BinaryAlexNet",
-    "BiRealNet",
-    "BinaryResNetE18",
-    "BinaryDenseNet28",
-    "BinaryDenseNet37",
-    "BinaryDenseNet37Dilated",
-    "BinaryDenseNet45",
-    "DoReFaNet",
-    "XNORNet",
-    "QuickNet",
-    "datasets",
-    "decode_predictions",
-    "preprocess_input",
-]
+__all__ = ["datasets", "decode_predictions", "preprocess_input", "literature", "sota"]
