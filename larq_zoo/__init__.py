f19a46cce9668d218f901d4072c7f2b495298037
James Widdicombe <jamescook.106@gmail.com>
2020-02-21 14:40:36 +0000

    black for _init_

--- larq_zoo/__init__.py
+++ larq_zoo/__init__.py
@@ -2,8 +2,12 @@ from larq_zoo import datasets
 from larq_zoo.binary_alex_net import BinaryAlexNet
 from larq_zoo.birealnet import BiRealNet
 from larq_zoo.data import preprocess_input
-from larq_zoo.densenet import (BinaryDenseNet28, BinaryDenseNet37,
-                               BinaryDenseNet37Dilated, BinaryDenseNet45)
+from larq_zoo.densenet import (
+    BinaryDenseNet28,
+    BinaryDenseNet37,
+    BinaryDenseNet37Dilated,
+    BinaryDenseNet45,
+)
 from larq_zoo.dorefanet import DoReFaNet
 from larq_zoo.quicknet import QuickNet
 from larq_zoo.resnet_e import BinaryResNetE18
