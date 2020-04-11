da01ea43aaf1c25e1f9f7673a42320267d07848b
Michael Tinsley <michaeltinsley@outlook.com>
2020-04-11 12:50:53 +0100

    Update xnornet.py

--- larq_zoo/literature/xnornet.py
+++ larq_zoo/literature/xnornet.py
@@ -148,6 +148,12 @@ def XNORNet(
     /plots/xnornet.vg.json
     ```
 
+    # ImageNet Metrics
+    Top-1 Accuracy: 44.96 %
+    Top-5 Accuracy: 69.18 %
+    Parameters: 62 396 768
+    Memory: 22.81 MB
+
     # Arguments
     input_shape: Optional shape tuple, to be specified if you would like to use a model
         with an input image resolution that is not (224, 224, 3).
