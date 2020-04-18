1fdba40e94b77a4390061d1fdf7dce416265eea3
timdebruin <timdebruin89@gmail.com>
2020-04-18 12:14:36 +0100

    Add test (WIP)

--- tests/train_test.py
+++ tests/train_test.py
@@ -5,7 +5,8 @@ import tensorflow_datasets as tfds
 from click.testing import CliRunner
 from zookeeper.tf.dataset import TFDSDataset
 
-from larq_zoo.training import basic_experiments
+from larq_zoo.training import basic_experiments, multi_stage_experiments
+from tests import dummy_datasets  # noqa
 
 
 @pytest.mark.parametrize("command", list(basic_experiments.cli.commands.keys()))
@@ -25,5 +26,21 @@ def test_cli(_, command):
             "--no-save_weights",
         ],
         catch_exceptions=False,
+    )
+    assert result.exit_code == 0
+
+
+@pytest.mark.parametrize("command,phases", [("TrainR2BStrongBaseline", 2)])
+def test_multi_stage_experiments(command, phases):
+    arguments = [command] + ["--no-use_tensorboard", "--no-use_model_checkpointing"]
+    for phase in range(phases):
+        arguments.extend([f'stage_{phase}.{c}' for c in ["dataset=DummyOxfordFlowers",
+            "epochs=1",
+            "batch_size=2",
+            "validation_frequency=5",]])
+
+    result = CliRunner().invoke(
+        multi_stage_experiments.cli,
+        args=arguments
     )
     assert result.exit_code == 0
