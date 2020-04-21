ba33a4abb6301ad91373241b32a6c554a8496028
koenhelwegen <koen@plumerai.com>
2020-04-21 08:14:24 +0100

    isort

--- larq_zoo/literature/__init__.py
+++ larq_zoo/literature/__init__.py
@@ -7,9 +7,9 @@ from larq_zoo.literature.densenet import (
     BinaryDenseNet45,
 )
 from larq_zoo.literature.dorefanet import DoReFaNet
+from larq_zoo.literature.meliusnet import MeliusNet22
 from larq_zoo.literature.real_to_bin_nets import RealToBinaryNet
 from larq_zoo.literature.resnet_e import BinaryResNetE18
-from larq_zoo.literature.meliusnet import MeliusNet22
 from larq_zoo.literature.xnornet import XNORNet
 
 __all__ = [
