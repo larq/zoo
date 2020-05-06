2fb464d52745b1372ce51bdd595551a94b4b1a18
Jelmer <jelmer@plumerai.com>
2020-05-06 08:29:18 +0200

    Compare lowercase summaries

--- tests/models_test.py
+++ tests/models_test.py
@@ -1,12 +1,11 @@
 from pathlib import Path
 
 import larq as lq
+import larq_zoo as lqz
 import numpy as np
 import pytest
 import tensorflow as tf
 from zookeeper import cli
-
-import larq_zoo as lqz
 
 
 @pytest.fixture(autouse=True)
@@ -113,7 +112,7 @@ def test_model_summary(app, last_feature_dim):
     if summary_file.exists():
         with open(summary_file, "r") as file:
             content = file.read()
-        assert content == capture.output
+        assert content.lower() == capture.output.lower()
     else:
         with open(summary_file, "w") as file:
             file.write(capture.output)
