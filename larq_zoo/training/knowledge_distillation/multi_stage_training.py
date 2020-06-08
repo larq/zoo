c9e96a33380b4b8ea2e96c0c9ad63372b24e6352
Tim de Bruin <tim@plumerai.com>
2020-06-08 18:02:53 +0100

    Type hints update (#175)
    * type hints fix

--- larq_zoo/training/knowledge_distillation/multi_stage_training.py
+++ larq_zoo/training/knowledge_distillation/multi_stage_training.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from pathlib import Path
-from typing import List, Optional, Union
+from typing import List, Optional
 
 import tensorflow as tf
 from zookeeper import ComponentField, Field
@@ -106,7 +106,7 @@ class LarqZooModelTrainingPhase(TrainingPhase):
     output_matching_train_teacher: bool = Field(False)
 
     @Field
-    def loss(self) -> Optional[Union[tf.keras.losses.Loss, str]]:
+    def loss(self):
         return getattr(self.__base_getattribute__("model"), "classification_loss")
 
     metrics = Field(lambda: ["accuracy", "sparse_top_k_categorical_accuracy"])
