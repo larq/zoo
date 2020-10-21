517876b34e0f4f5e696f11919d0ba934014b7919
Adam Hillier <7688302+AdamHillier@users.noreply.github.com>
2020-10-21 18:51:50 +0100

    Fix package in version.

--- larq_zoo/__init__.py
+++ larq_zoo/__init__.py
@@ -9,6 +9,6 @@ except ImportError:
     # Running on pre-3.8 Python; use importlib-metadata package
     import importlib_metadata as metadata  # type: ignore
 
-__version__ = metadata.version("larq")
+__version__ = metadata.version("larq_zoo")
 
 __all__ = ["datasets", "decode_predictions", "preprocess_input", "literature", "sota"]
