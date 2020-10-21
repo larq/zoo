7b0eac18ce07d01aa272e9bcd24aaf0adc91f64a
Adam Hillier <7688302+AdamHillier@users.noreply.github.com>
2020-10-21 18:40:32 +0100

    Add __version__ property.

--- larq_zoo/__init__.py
+++ larq_zoo/__init__.py
@@ -3,4 +3,12 @@ from larq_zoo.core.utils import decode_predictions
 from larq_zoo.training import datasets
 from larq_zoo.training.data import preprocess_input
 
+try:
+    from importlib import metadata  # type: ignore
+except ImportError:
+    # Running on pre-3.8 Python; use importlib-metadata package
+    import importlib_metadata as metadata  # type: ignore
+
+__version__ = metadata.version("larq")
+
 __all__ = ["datasets", "decode_predictions", "preprocess_input", "literature", "sota"]
