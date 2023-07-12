8f43d9e4936b138977454e3011f9dbe694a4dfe5
Cedric Nugteren <web@cedricnugteren.nl>
2023-07-12 17:19:17 +0200

    :arrow_up: v2.3.2 (#446)
    Changelog will be made upon release.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name="larq-zoo",
-    version="2.3.1",
+    version="2.3.2",
     author="Plumerai",
     author_email="opensource@plumerai.com",
     description="Reference implementations of popular Binarized Neural Networks",
